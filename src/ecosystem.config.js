b8143b4c1070e8eb5e23c40ca1ade6456227c804
Saulo Maciel <saulomcm@gmail.com>
2024-09-06 09:52:29 -0300

    Atualiza o arquivo ecosystem

--- src/ecosystem.config.js
+++ src/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'my-app',
-      script: './server.js',
+      script: '../dist/server.js',
       instances: 'max',
       exec_mode: 'cluster',
       env: {
@@ -20,7 +20,13 @@ module.exports = {
       ref: 'origin/main',
       repo: 'git@github.com:saulomaciel1991/miniserver.git',
       path: '/var/www/my-app',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+      'pre-deploy': 'rm -rf /var/www/my-app/source && git clone git@github.com:saulomaciel1991/miniserver.git /var/www/my-app/source',
+      'post-deploy': 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" && ' +
+                     '[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh" && ' +
+                     'nvm use 20.17.0 && ' +
+                     'cd /var/www/my-app/source && ' +
+                     'npm install && ' +
+                     'pm2 reload ecosystem.config.js --env production'
     }
   }
 };
